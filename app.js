21073541220023c63c3c152722bf2cb258756f03
lux_aeterna <Jackziz@users.noreply.github.com>
2022-01-29 00:30:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars');
-
-
-const host = '127.0.0.1';
-const port = 8080;
-
-app.enable('view cache');
-
-app.get('/', function(req, res) {
-  res.send('Hello world!');
-});
-
-
-
-app.listen(port, host, function () {
-  console.log(`Server listens http://${host}:${port}`)
-});
+const PORT = process.env.PORT || 8080;
+app.get('/',(req, res) => res.send('Hello World'));
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
